f02ee9c960f80a828924a30952d52211b852490d
Documenter.jl <documenter@juliadocs.github.io>
2022-05-10 09:24:36 +0000

    build based on 6ab17a3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ImageStitching","category":"page"},{"location":"#ImageStitching","page":"Home","title":"ImageStitching","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ImageStitching.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ImageStitching]","category":"page"}]
+}
